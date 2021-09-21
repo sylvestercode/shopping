636532ad36af7336c956cc93af23cbe82e950ca8
sylvestercode <wetechhubs@gmail.com>
2021-09-21 11:00:50 -0700

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+const data = {
+
+products:[
+
+{
+
+ _id:'1',
+ name: 'nike slim shirt',
+ category: 'shirts',
+ image:'images/productimg.jpg',
+ price:120,
+ brand: 'Nike',
+ rating: 4.5,
+ numReviews: 10,
+ description: 'high quality products from mazi',
+
+},
+
+{
+
+    _id:'2',
+    name: 'bottle two face',
+    category: 'shirts',
+    image:'images/productimg.jpg',
+    price:110,
+    brand: 'Nike',
+    rating: 4.0,
+    numReviews: 10,
+    description: 'high quality products from mazi',
+   
+   },
+   
+{
+
+    _id:'3',
+    name: 'nike slim pant',
+    category: 'shirts',
+    image:'images/productimg.jpg',
+    price:125,
+    brand: 'Nike',
+    rating: 5.0,
+    numReviews: 10,
+    description: 'high quality products from mazi',
+   
+   },
+   
+{
+
+    _id:'4',
+    name: 'nike bottle slim',
+    category: 'shirts',
+    image:'images/productimg.jpg',
+    price:130,
+    brand: 'Nike',
+    rating: 5.0,
+    numReviews: 10,
+    description: 'high quality products from mazi',
+   
+   },
+   
+{
+
+ _id:'5',
+ name: 'nike slim shirts',
+ category: 'shirts',
+ image:'images/productimg.jpg',
+ price:115,
+ brand: 'Nike',
+ rating: 4.3,
+ numReviews: 10,
+ description: 'high quality products from mazi',
+
+},
+
+
+
+
+
+
+],
+
+};
+
+export default data;
